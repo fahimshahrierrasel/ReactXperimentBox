1836e664b06a5b3f9ac379094a662e646c813b9a
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-11-07 23:57:12 +0600

    text field separate component add

--- src/views/pages/inputs/inputs.js
+++ src/views/pages/inputs/inputs.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./inputs.scss";
+import TextField from "../../components/text-field";
 
 const Inputs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <div className="inputs__container">
-      <div className="form-field">
-        <input type="email" id="name" name="name" placeholder=" " />
-        <label htmlFor="name" className="form-field__label">
-          Name
-        </label>
+      <div className="form-control">
+        <TextField
+          title="Name"
+          type="text"
+          value={name}
+          onChange={e => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-control">
+        <TextField
+          title="Email"
+          type="email"
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
